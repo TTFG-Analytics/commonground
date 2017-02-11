4c402c977525bf6213096cddec9a3b9bbc4d567d
Greg Bacus <gbacus28@gmail.com>
2017-02-11 17:25:04 -0600

    completed mock front-end with integrated database

--- server/index.js
+++ server/index.js
@@ -28,10 +28,28 @@ app.post('/import', function(req,res){
 })
 
 app.post('/discuss', function(req,res){
-  console.log("body", req.body)
-  console.log("user", currentUser);
-  // knex('discussion').insert({input: req.body.discussinput})
-  // .then(function(){});
+  console.log(req.body);
+
+  knex('discussion').returning('id').insert({input: req.body.topic, user_id: currentUser.id})
+  .then(function(data){
+    console.log(data);
+    knex('commonground').insert({input: req.body.commonground1, discussion_id: data[0], user_id: currentUser.id}).then(function(){})
+    knex('commonground').insert({input: req.body.commonground2, discussion_id: data[0], user_id: currentUser.id}).then(function(){})
+  })
+})
+
+app.post('/comment', function(req,res){
+  console.log(req.body);
+
+  knex('comment').insert({input: req.body.comment, user_id: currentUser.id, commonground_id: req.body.commongroundId }).then(function(){});
+
+})
+
+app.post('/vote', function(req,res){
+  console.log(req.body);
+
+  knex('vote').insert({input: req.body.vote, user_id: currentUser.id, comment_id: req.body.commentId }).then(function(){});
+
 })
 
 
