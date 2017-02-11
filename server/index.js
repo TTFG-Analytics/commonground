7673e516d37830bbd943c34976b53d012905b7cd
Greg Bacus <gbacus28@gmail.com>
2017-02-11 15:43:14 -0600

    working on /dicuss route

--- server/index.js
+++ server/index.js
@@ -7,47 +7,35 @@ app.use(bodyParser.urlencoded());
 
 module.exports = app;
 
+//This mimics a user session being saved
+var currentUser;
+
 var knex = require('knex')({
-    client: 'postgresql',
-    connection: {
-      database: 'cg_db',
-      user:     'Greg',
-      password: 'commonground'
-    }
-  });
+  client: 'postgresql',
+  connection: {
+    database: 'cg_db',
+    user:     'Greg',
+    password: 'commonground'
+  }
+});
 
 app.post('/import', function(req,res){
-  console.log("this is the req.body: ", req.body);
 
-  console.log("this route was hit");
+  currentUser = req.body;
 
   knex('users').returning('id').insert({name: req.body.name, age: req.body.age, hometown: req.body.hometown, gender: req.body.gender, race: req.body.race, occupation: req.body.occupation, politicalleaning:req.body.politicalleaning, religion: req.body.religion, yearlyincome:req.body.yearlyincome})
-  .then(function(data){
-    console.log('here is the req', req.body);
+  .then(function(data){ currentUser.id = data[0] });
+})
 
-    console.log("this is the data", data[0], Array.isArray(data));
-    knex('discussion').insert({input: req.body.discussinput, user_id: data[0]})
-    .then(function(data){ console.log("ending data", data) });
-  })
-
-
-
-    // function(data){console.log("this is what is being returned", data)});
-
-
-
-
-
-  // knex.raw(`INSERT INTO users (name, age, hometown, gender, race, occupation, politicalleaning, religion, yearlyincome) VALUES ('${req.body.name}',${req.body.age},'${req.body.hometown}',${req.body.gender},${req.body.race},${req.body.occupation},${req.body.politicalleaning},${req.body.religion},${req.body.yearlyincome})`).then(function(rows){console.log('Inserted or Replaced!', rows)});
-
-  // knex.raw(`INSERT INTO discussion (input) VALUES ('${req.body.discussinput}',${req.body.user_id})`).then(function(rows){console.log('Inserted or Replaced!', rows)});
-
-  res.status(200).send("random string");
+app.post('/discuss', function(req,res){
+  console.log("body", req.body)
+  console.log("user", currentUser);
+  // knex('discussion').insert({input: req.body.discussinput})
+  // .then(function(){});
 })
 
 
 app.get('/', function (req, res) {
-  console.log('JUST REFRESHED!');
   res.sendFile(path.resolve(__dirname + '/../public/index.html'));
 });
 
