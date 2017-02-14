166214c4a87e7a120329564d29f6c5fa00e35484
Forrest Miller <forrest.w.miller@icloud.com>
2017-02-14 12:16:15 -0600

    finishing up on seed

--- db/migrations/20170210133122_create_all_tables.js
+++ db/migrations/20170210133122_create_all_tables.js
@@ -36,8 +36,13 @@ exports.up = function(knex, Promise) {
     knex.schema.createTable('comment', function(t) {
       t.increments('id').unsigned().primary();
       t.string('input', 2000).notNull();
+<<<<<<< HEAD
       t.integer('upvotecounter').defaultTo(0);
       t.integer('downvotecounter').defaultTo(0);
+=======
+      t.integer('upvoteCounter')
+      t.integer('downvoteCounter')
+>>>>>>> finishing up on seed
       t.timestamp('createdat').defaultTo(knex.fn.now());
       t.integer('commonground_id')
       t.integer('user_id')
@@ -47,7 +52,11 @@ exports.up = function(knex, Promise) {
 
     knex.schema.createTable('vote', function(t) {
       t.increments('id').unsigned().primary();
+<<<<<<< HEAD
       t.boolean('input');
+=======
+      t.boolean('input').notNull();
+>>>>>>> finishing up on seed
       t.timestamp('createdat').defaultTo(knex.fn.now());
       t.integer('comment_id')
       t.integer('user_id')
