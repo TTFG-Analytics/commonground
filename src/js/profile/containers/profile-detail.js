33f7d11829af47b28847cacfce6cf4a6a4b94347
Tin <tinytheyfon8@gmail.com>
2017-02-23 16:34:34 -0600

    Stuck at async/making input fields editable

--- src/js/profile/containers/profile-detail.js
+++ src/js/profile/containers/profile-detail.js
@@ -4,34 +4,54 @@ import {bindActionCreators} from 'redux';
 import {getProfile} from '../actions/profileActions';
 import Input from 'react-toolbox/lib/input';
 import Dropdown from 'react-toolbox/lib/dropdown';
+import {Button, IconButton} from 'react-toolbox/lib/button';
 
 class UserProfile extends Component {
   constructor(props){
     super(props);
 
+
+    console.log('props:', props);
     this.state = {
-      name: this.props.profile.name,
-      title: this.props.profile.title,
-      hometown: this.props.profile.hometown,
-      gender: this.props.profile.gender,
-      race: this.props.profile.race,
-      industry: this.props.profile.industry,
-      politicalLeaning: this.props.profile.politicalleaning,
-      religion: this.props.profile.religion,
-      yearlyIncome: this.props.profile.yearlyincome
-    }
+      name: 'Tin',
+      title: '',
+      hometown: 'Indiana',
+      gender: {value: 'male'},
+      race: {value: 'race-1'},
+      industry: {value: 'industry-1'},
+      politicalLeaning: {},
+      religion: {value: 'religion-1'},
+      yearlyIncome: {}
+      // title: this.props.profile.title,
+      // hometown: this.props.profile.hometown,
+      // gender: this.props.profile.gender,
+      // race: this.props.profile.race,
+      // industry: this.props.profile.industry,
+      // politicalLeaning: this.props.profile.politicalleaning,
+      // religion: this.props.profile.religion,
+      // yearlyIncome: this.props.profile.yearlyincome
+    };
 
-
-
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  componentDidMount() {
+  componentWillMount() {
     this.props.getProfile();
+  }
 
+  //temp just to see if name can be changeable
+  handleName(value) {
+    this.setState({
+      name: value
+    })
   }
 
   handleChange(value) {
-    this.setState({gender: value});
+    // this.setState({gender: value});
+
+    this.setState({
+      gender: value
+    })
   }
 
   render () {
@@ -105,31 +125,39 @@ class UserProfile extends Component {
 
     const income = [
       {value: 'income-1', label: 'Under $35,000 / year'},
-      {value: 'income-1', label: '$35,000 - $50,000'},
-      {value: 'income-1', label: '$50,000 - $65,000'},
-      {value: 'income-1', label: '$65,000 - $80,000'},
-      {value: 'income-1', label: '$80,000 - $95,000'},
-      {value: 'income-1', label: '$95,000 - $120,000'},
-      {value: 'income-1', label: 'Over $120,000'},
+      {value: 'income-2', label: '$35,000 - $50,000'},
+      {value: 'income-3', label: '$50,000 - $65,000'},
+      {value: 'income-4', label: '$65,000 - $80,000'},
+      {value: 'income-5', label: '$80,000 - $95,000'},
+      {value: 'income-6', label: '$95,000 - $120,000'},
+      {value: 'income-7', label: 'Over $120,000'},
       ];
 
+      this.state.name = this.props.profile.name;
+
+      console.log('djsakdjask', this.state);
     return (
       <div>
       <p>Name: </p>
-      <Input type='text' value={this.state.name}/>
+      <Input type='text' name='name' value={this.state.name} onChange={this.handleName.bind(this)}/>
+      <p>Title: </p>
+      <Input type='text' name='title' value={this.state.title}/>
+      <p>Hometown: </p>
+      <Input type='text' name='hometown' value={this.state.hometown}/>
       <p>Gender: </p>
-      <Dropdown auto onChange={this.handleChange} source={gender} value={this.state.value} />
+      <Dropdown name='gender' auto onChange={this.handleChange} source={gender} value={this.state.gender} />
       <p>Race: </p>
-      <Dropdown auto onChange={this.handleChange} source={race} value={this.state.value} />
+      <Dropdown name='race' auto onChange={this.handleChange} source={race} value={this.state.race} />
       <p>Political Leaning: </p>
-      <Dropdown auto onChange={this.handleChange} source={politicalleaning} value={this.state.value} />
+      <Dropdown auto onChange={this.handleChange} source={politicalleaning} value={this.state.politicalLeaning} />
       <p>Industry: </p>
-      <Dropdown auto onChange={this.handleChange} source={industry} value={this.state.value} />
+      <Dropdown auto onChange={this.handleChange} source={industry} value={this.state.industry} />
       <p>Religion: </p>
-      <Dropdown auto onChange={this.handleChange} source={religion} value={this.state.value} />
+      <Dropdown auto onChange={this.handleChange} source={religion} value={this.state.religion} />
       <p>Income: </p>
-      <Dropdown auto onChange={this.handleChange} source={income} value={this.state.value} />
+      <Dropdown auto onChange={this.handleChange} source={income} value={this.state.income} />
 
+      <Button type='submit' label='Submit' raised primary/>
       </div>
     )
   }
