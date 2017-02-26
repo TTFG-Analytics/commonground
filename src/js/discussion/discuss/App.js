97fad578744f68ae86e48027736d9538c69f859b
Tai Hovanky <tai.hovanky@gmail.com>
2017-02-26 13:38:09 -0600

    cleaned up App.js

--- src/js/discussion/discuss/App.js
+++ src/js/discussion/discuss/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Discussion from './Discussion'
 import AddDiscussion from './AddDiscussion'
 import DiscussionList from './DiscussionList'
 import { getDiscussions } from '../actions/actions'
@@ -7,11 +6,8 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
 
 //import ProfileButton from '../camps/ProfileButton'
-//import { AppBar } from 'react-toolbox/lib/app_bar'
 import { Link } from 'react-router'
 import Navigation from '../../navbar/navbar'
-
-
 
 DiscussionList.need = [
   getDiscussions
@@ -32,14 +28,8 @@ class App extends React.Component{
 
 //the section below is used (along side the .need above) to have the App
 //component dispatch the getDiscussions axios call before rendering DiscussionList
-const mapStateToProps = (state) => {
-  return {
-    discussionsList: state.discussionsGet
-  }
-}
-
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators(getDiscussions, dispatch)
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(null, mapDispatchToProps)(App)
