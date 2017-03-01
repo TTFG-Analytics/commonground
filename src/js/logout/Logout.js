a881baf6f05a95b8b1b7b82507c4147c45fc131e
Tai Hovanky <tai.hovanky@gmail.com>
2017-03-01 15:35:41 -0600

    user setup modification and css update

--- src/js/logout/Logout.js
+++ src/js/logout/Logout.js
@@ -1,19 +1,19 @@
 import React from 'react'
-// import { Card, CardMedia, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card';
-import FaceBookIntegration from '../discussion/camps/FaceBookIntegration'
+import FaceBookButton from '../discussion/camps/FaceBookButton'
 import store from '../store'
-// import { AppBar } from 'react-toolbox/lib/app_bar'
 import BackButton from '../discussion/camps/BackButton'
 import ProfileButton from '../discussion/camps/ProfileButton'
-import { Link } from 'react-router'
+import Router from 'react-router'
 import Navigation from '../navbar/navbar'
 
 class Logout extends React.Component{
+  
   render() {
     return (
+
       <div>
         <Navigation />
-        <FaceBookIntegration />
+        <FaceBookButton />
       </div>
     )
   }
