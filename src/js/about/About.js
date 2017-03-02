a4555780b1f30903edd1177ade6729cca1e54e9d
Forrest Miller <forrest.w.miller@icloud.com>
2017-03-02 11:47:49 -0600

    initial styling for about page done

--- src/js/about/About.js
+++ src/js/about/About.js
@@ -1,34 +1,33 @@
 const React = require('react');
 import Navigation from '../navbar/navbar'
+import { Panel, Col, Grid, Accordion } from 'react-bootstrap';
+
 
 class About extends React.Component{
   render(){
     return (
       <div>
       <Navigation />
-      <h1>About</h1>
-
-        <div>
-          <h3>What this app is about</h3>
-          <p>
+      <Col md={8} mdOffset={2}>
+      <Panel> 
+        <Accordion>
+          <Panel header="What this app is about" eventKey="1">
             This is a social app designed to facilitate comunication between different viewpoints.
-          </p>
-          <h3>How to use this app</h3>
-          <p>
+          </Panel>
+          <Panel header="How to use this app" eventKey="2">
             By signing in through facebook, you will be able to create a discussion, create a comment, or vote on a post.
             However you are not required to be signed in to browse content. If there are multiple users on your computer,
             there is an ability to logout and login with a different facebook ID. Please note that this will also log you
             out of facebook proper.
-          </p>
-          <h3>Rules</h3>
-          <p>
-            <ul>
+          </Panel>
+          <Panel header="Rules" eventKey="3">
+            <ol>
               <li>No ad hominem attacks</li>
               <li>Be nice and civil</li>
-            </ul>
-          </p>
-          <h3>How we use your data</h3>
-          <p>
+            </ol>
+          </Panel>          
+
+          <Panel header="How we use your data" eventKey="4">
             Nunc orci nulla, iaculis a nulla vel, imperdiet rutrum nulla. Sed gravida, 
             lectus eget mattis viverra, dui mauris suscipit dolor, quis molestie massa 
             purus at diam. Fusce lobortis ullamcorper sem ac feugiat. Cras maximus mi 
@@ -36,13 +35,14 @@ class About extends React.Component{
             porta diam. Fusce euismod, dolor et dictum tincidunt, magna est aliquam 
             elit, ac ultrices erat quam et nibh. Suspendisse luctus posuere ante. 
             Vivamus in orci id lacus imperdiet suscipit ac ut nisi.
-          </p>
-          <h3>About Us</h3>
-          <p>
+          </Panel>
+          
+          <Panel header="About Us" eventKey="5">
             TTFG are a group of like minded software engineers hoping to make the world a better place, one app at a time.
-          </p>
-
-        </div>
+          </Panel>
+        </Accordion>
+        </Panel>
+      </Col>  
       </div>
     )
   }
