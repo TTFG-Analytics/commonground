64b3e332ba30bb3aa0c17fb1af4c594248ac43b8
Tai Hovanky <tai.hovanky@gmail.com>
2017-03-02 00:53:51 -0600

    user can close modal and not overwrite contribution

--- src/js/discussion/camps/Constraint.js
+++ src/js/discussion/camps/Constraint.js
@@ -18,6 +18,12 @@ class Constraint extends React.Component{
     })
   }
 
+  doNothing() {
+    this.setState({
+      showModal: !this.props.showModal
+    })
+  }
+
   allowContribution() {
     console.log('allowContribution this', this)
     this.props.contributeAgain()
@@ -35,7 +41,8 @@ class Constraint extends React.Component{
           </div>
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={() => this.allowContribution()} type='submit' bsStyle="primary">Delete old contribution and Add new</Button>
+          <Button onClick={() => this.doNothing()} type='submit' bsStyle='default'>Keep old contribution and do nothing</Button>
+          <Button onClick={() => this.allowContribution()} type='submit' bsStyle="warning">Delete old contribution and Add new</Button>
         </Modal.Footer>
       </Modal>
     )
